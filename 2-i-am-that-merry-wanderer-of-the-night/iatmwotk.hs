ed462922f88aecc4586fcfed3e6e508d15ff45c3
Moss Collum <moss.collum@gmail.com>
2014-02-04 22:48:57 -0500

    Refactoring.

--- 2-i-am-that-merry-wanderer-of-the-night/iatmwotk.hs
+++ 2-i-am-that-merry-wanderer-of-the-night/iatmwotk.hs
@@ -2,12 +2,15 @@ module IAmThatMerryWandererOfTheNight where
 import System.Console.ANSI
 import System.IO
 
-main :: IO ()
-main = do
+initScreen = do
     hSetBuffering stdin NoBuffering
     hSetBuffering stdout NoBuffering
     hSetEcho stdin False
     clearScreen
+
+main :: IO ()
+main = do
+    initScreen
     setCursorPosition 12 40
     putChar '@'
     setCursorPosition 26 0
