2163c3aa6f14051bc2360de0d7a1694b54ef73a3
Moss Collum <moss.collum@gmail.com>
2014-05-01 10:19:16 -0400

    Move GameView typeclass implementation to rfk.hs

--- infinite-future-roguelike-exploration/src/Rendering.hs
+++ infinite-future-roguelike-exploration/src/Rendering.hs
@@ -2,7 +2,6 @@ module Rendering where
 
 import System.Console.ANSI
 import System.IO
-
 import Model
 
 class GameView a where
@@ -27,7 +26,7 @@ updateScreen (oldState, newState) = do
 
 type Renderer = UiElement -> IO ()
 
-renderWith :: Renderer -> GameState -> IO ()
+renderWith :: GameView a => Renderer -> a -> IO ()
 renderWith renderer gameState = mapM_ renderer $ view gameState
 
 drawElement :: UiElement -> IO ()
@@ -45,12 +44,3 @@ clearElement (Message _) = do
 data UiElement = Sprite Char Position
                | Message String
                deriving (Show, Eq)
-
-instance GameView GameState where
-  -- |view breaks up a GameState into UiElements
-  -- >>> view Playing { robot = (3,4), message = "Hi!", over = False, level = [NKI 'x' (5,6) "a duck", Kitten 'k' (7,8)] }
-  -- [Sprite 'x' (5,6),Sprite 'k' (7,8),Sprite '#' (3,4),Message "Hi!"]
-  view Playing { robot = r, message = m, level = l } = (map itemView l) ++ [Sprite '#' r, Message m] 
-
-itemView (Kitten representation position) = Sprite representation position
-itemView (NKI representation position _) = Sprite representation position
--- infinite-future-roguelike-exploration/src/rfk.hs
+++ infinite-future-roguelike-exploration/src/rfk.hs
@@ -6,6 +6,15 @@ import Update
 import LevelGeneration
 import Rendering
 
+instance GameView GameState where
+  -- |view breaks up a GameState into UiElements
+  -- >>> view Playing { robot = (3,4), message = "Hi!", over = False, level = [NKI 'x' (5,6) "a duck", Kitten 'k' (7,8)] }
+  -- [Sprite 'x' (5,6),Sprite 'k' (7,8),Sprite '#' (3,4),Message "Hi!"]
+  view Playing { robot = r, message = m, level = l } = (map itemView l) ++ [Sprite '#' r, Message m] 
+
+itemView (Kitten representation position) = Sprite representation position
+itemView (NKI representation position _) = Sprite representation position
+
 -- TODO this is, frankly, also a bit of a hack
 transitions :: [a] -> [(a, a)]
 transitions list = zip ([head list] ++ list) list
